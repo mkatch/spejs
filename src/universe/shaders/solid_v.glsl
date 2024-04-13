32f6cd07cfdb66bf5031fb6692b595cda337bc7c
Marcin Kaczmarek <mkacz91@gmail.com>
2024-04-13 14:25:43 +0200

    Solid shader experiment

--- src/universe/shaders/solid_v.glsl
+++ src/universe/shaders/solid_v.glsl
@@ -1,13 +1,18 @@
 #version 460
 
-uniform mat4 projection;
-uniform mat4 model;
+uniform mat4 Projection;
+uniform mat4 Model;
+uniform mat3 _Normal_model;
 in vec3 position;
 in vec3 normal;
+
+out vec3 frag_position;
 out vec3 frag_normal;
 
 void main()
 {
-  frag_normal = normal;
-  gl_Position = projection * model * vec4(position, 1.0);
+  vec4 model_position = Model * vec4(position, 1.0);
+  frag_normal = _Normal_model * normal;
+  frag_position = model_position.xyz;
+  gl_Position = Projection * model_position;
 }
